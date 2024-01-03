553bdb0bd1dab1d02fdc5e212b619933b47b0ca3
jennifer-90 <jennifer.bastin.90@gmail.com>
2024-01-03 20:54:55 +0100

    vueGuest.modif

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
@@ -14,8 +15,12 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
+
+
+
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
+
             @include('layouts.navigation')
 
             <!-- Page Heading -->
@@ -32,5 +37,6 @@
                 {{ $slot }}
             </main>
         </div>
+
     </body>
 </html>
